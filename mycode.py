56f8faa80f2879a1be9e6f631877bac526e53927
kaykypg7 <112106334+kaykypg7@users.noreply.github.com>
2023-10-30 14:50:47 +0000

    terminei

--- mycode.py
+++ mycode.py
@@ -1,3 +1,5 @@
+import os
+
 idade = int (input("digite sua idade"))
 total = float(input('digite o valor total do produto'))
 
@@ -9,3 +11,10 @@ if(idade >= 18 ):
     o valor do desconto foi de {desconto}, 
     e o total é de {valorFinal}
     """)
+
+
+else:
+    os.system("clear")
+    print(f"não tem direito ao desconto e o seu valor final é de R$: {total:.2f}")
+
+    
